4a64009424c9f144a6494e7f3429393ab254c159
jasperBailey <jasperbailey98@gmail.com>
2023-08-30 03:03:08 +0100

    added codecast link

--- src/App.js
+++ src/App.js
@@ -5,18 +5,26 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi there! I'm Jasper, and currently working on making this portfolio website a bit better. As of 30th August 2023, it's a little quick and dirty at the moment, but check back in a few days to see what I've done with the place! Currently I'm working on migrating my previously locally hosted projects to being web hosted so you can have a look and play around with them.
         </p>
+        <h3>
+          Projects:
+        </h3>
+        <h6>
+          CodeCast
+        </h6>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        href="https://codecast.jasperbailey.co.uk">
+          Link to web-hosted version
         </a>
+        <p>
+          Unfortunately they changed the rate limit for the Taddy Podcast API after we made this - so if the search stops working, then I'm sorry! Too many people must be looking at it, and I don't want to buy a more expensive API key.
+
+          This was originally designed to be locally hosted, as we had only been learning JavaScript for 3 weeks at the time and hadn't covered web hosting yet.
+
+          It uses a React front-end, with a mix of regular CSS sheets and Styled Components. The back-end is an Express.js server with a REST API, connected to a PostgreSQL database. I haven't yet started hosting the back-end as I need to modify it to work with the web-hosted front-end!
+        </p>
       </header>
     </div>
   );
