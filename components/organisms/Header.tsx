771bb365d83c4b7519e1b1a3ffd818ca6ed742a7
Jasper Bailey <jasperbailey98@gmail.com>
2023-09-18 16:56:28 +0100

    added components

---
+++ components/organisms/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <>
+      <header>
+        <img
+          src="https://previews.123rf.com/images/vgstudio/vgstudio1905/vgstudio190500026/122167555-man-portrait-young-happy-man-with-smiling-face-male-model-in-crossed-arms-pose-blue-background-guy.jpg"
+          className="h-48"
+        />
+      </header>
+    </>
+  );
+};
+
+export default Header;
