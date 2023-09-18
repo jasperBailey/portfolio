2a5f789be2b4a27a94869d742c3af978c9643a56
Jasper Bailey <jasperbailey98@gmail.com>
2023-09-18 11:55:27 +0100

    added component files

---
+++ src/pages/main.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <p>hi</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
