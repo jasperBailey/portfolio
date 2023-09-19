0aba768645d54eb85ee73755200be429c6d8ab6d
Jasper Bailey <jasperbailey98@gmail.com>
2023-09-19 16:59:00 +0100

    added social links

--- components/molecules/Socials.tsx
+++ components/molecules/Socials.tsx
@@ -2,9 +2,15 @@ const Socials = () => {
   return (
     <>
       <ul className="flex flex-row mt-3">
-        <li className="mr-5">Github</li>
-        <li className="mr-5">Email</li>
-        <li className="mr-5">LinkedIn</li>
+        <li className="mr-5">
+          <a href="https://www.github.com/jasperBailey">Github</a>
+        </li>
+        <li className="mr-5">
+          <a href="mailto:jasperbailey98@gmail.com">Email</a>
+        </li>
+        <li className="mr-5">
+          <a href="https://www.linkedin.com/in/jasperbailey">LinkedIn</a>
+        </li>
       </ul>
     </>
   );
