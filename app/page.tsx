ed7f030c2448bb4f1a7c0aa0b8b8c448eafa08ba
Jasper Bailey <jasperbailey98@gmail.com>
2023-09-19 17:10:10 +0100

    changed alignment on small screen sizes

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24">
       <p>Look at GitHub instead for the moment! Project previews in progress!</p>
-        {/*
+        /*
         <p>
           Unfortunately they changed the rate limit for the Taddy Podcast API after we made this - so if the search stops working, then I'm sorry! Too many people must be looking at it, and I don't want to buy a more expensive API key.
 
           This was originally designed to be locally hosted, as we had only been learning JavaScript for 3 weeks at the time and hadn't covered web hosting yet.
-        </p> */}
-    </main>
+        </p> */
   )
 }
