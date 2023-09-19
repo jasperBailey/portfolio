daaaaf97fabe77f320e361b6deacde9deb2438ae
Jasper Bailey <jasperbailey98@gmail.com>
2023-09-19 17:02:57 +0100

    triggering redeploy

--- components/molecules/Socials.tsx
+++ components/molecules/Socials.tsx
@@ -1,18 +1,16 @@
 const Socials = () => {
   return (
-    <>
-      <ul className="flex flex-row mt-3">
-        <li className="mr-5">
-          <a href="https://www.github.com/jasperBailey">Github</a>
-        </li>
-        <li className="mr-5">
-          <a href="mailto:jasperbailey98@gmail.com">Email</a>
-        </li>
-        <li className="mr-5">
-          <a href="https://www.linkedin.com/in/jasperbailey">LinkedIn</a>
-        </li>
-      </ul>
-    </>
+    <ul className="flex flex-row mt-3">
+      <li className="mr-5">
+        <a href="https://www.github.com/jasperBailey">Github</a>
+      </li>
+      <li className="mr-5">
+        <a href="mailto:jasperbailey98@gmail.com">Email</a>
+      </li>
+      <li className="mr-5">
+        <a href="https://www.linkedin.com/in/jasperbailey">LinkedIn</a>
+      </li>
+    </ul>
   );
 };
 
