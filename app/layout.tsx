90ccb4b28dca1d96433c2c54397ea2d9796adb22
Jasper Bailey <jasperbailey98@gmail.com>
2023-09-20 11:53:14 +0100

    added FontAwesome icons for socials

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
 import Profile from '@/components/organisms/Profile'
 
 const inter = Inter({ subsets: ['latin'] })
--- components/molecules/Socials.tsx
+++ components/molecules/Socials.tsx
@@ -1,14 +1,36 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
 const Socials = () => {
   return (
     <ul className="flex flex-row mt-3">
       <li className="mr-5">
-        <a href="https://www.github.com/jasperBailey">Github</a>
+        <a href="https://www.github.com/jasperBailey">
+          <FontAwesomeIcon
+            icon={faGithub}
+            size="xl"
+            style={{ color: "#E2E8F0" }}
+          />
+        </a>
       </li>
       <li className="mr-5">
-        <a href="mailto:jasperbailey98@gmail.com">Email</a>
+        <a href="mailto:jasperbailey98@gmail.com">
+          <FontAwesomeIcon
+            icon={faEnvelope}
+            size="xl"
+            style={{ color: "#E2E8F0" }}
+          />
+        </a>
       </li>
       <li className="mr-5">
-        <a href="https://www.linkedin.com/in/jasperbailey">LinkedIn</a>
+        <a href="https://www.linkedin.com/in/jasperbailey">
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            size="xl"
+            style={{ color: "#E2E8F0" }}
+          />
+        </a>
       </li>
     </ul>
   );
